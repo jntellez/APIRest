5d4e2fa98f08b8011492624eec114e99fae3a4d7
jntellez <juantellez916@gmail.com>
2026-04-16 22:24:12 -0600

    Refactor code formatting for consistency and readability

--- main.py
+++ main.py
@@ -3,37 +3,58 @@ import uvicorn
 
 app = FastAPI()
 
+
 @app.get("/")
 def read_root():
-  return {"message": "Hello World"}
+    return {"message": "Hello World"}
+
 
 @app.post("/eventos", tags=["Eventos"], summary="Crear un evento")
 def crearEvento():
-  return {"message": "Evento creado"}
+    return {"message": "Evento creado"}
+
 
 @app.get("/eventos", tags=["Eventos"], summary="Listar eventos")
 def listarEventos():
-  return {"message": "Eventos listados"}
+    return {"message": "Eventos listados"}
+
 
 @app.get("/eventos/{idEvento}", tags=["Eventos"], summary="Consultar evento por ID")
 def consultarEventoPorID(idEvento: str):
-  return {"message": f"Evento con ID {idEvento} consultado"}
+    return {"message": f"Evento con ID {idEvento} consultado"}
 
-@app.put("/eventos/{idEvento}", tags=["Eventos"], summary="Modificar evento en base a su ID")
+
+@app.put(
+    "/eventos/{idEvento}", tags=["Eventos"], summary="Modificar evento en base a su ID"
+)
 def modificarEvento(idEvento: str):
-  return {"message": f"Evento con ID {idEvento} modificado"}
+    return {"message": f"Evento con ID {idEvento} modificado"}
 
-@app.get("/eventos/estatus/{estatus}", tags=["Eventos"], summary="Consultar eventos por estatus")
+
+@app.get(
+    "/eventos/estatus/{estatus}",
+    tags=["Eventos"],
+    summary="Consultar eventos por estatus",
+)
 def consultarEventosPorEstatus(estatus: str):
-  return {"message": f"Eventos con estatus {estatus} consultados"}
+    return {"message": f"Eventos con estatus {estatus} consultados"}
 
-@app.put("/eventos/estatus/{idEvento}", tags=["Eventos"], summary="Cambio de estatus de un evento")
+
+@app.put(
+    "/eventos/estatus/{idEvento}",
+    tags=["Eventos"],
+    summary="Cambio de estatus de un evento",
+)
 def cambioEstatusEvento(idEvento: str):
-  return {"message": f"Cambio de estatus de un evento con ID {idEvento}"}
+    return {"message": f"Cambio de estatus de un evento con ID {idEvento}"}
 
-@app.put("/eventos/reprogramar/{idEvento}", tags=["Eventos"], summary="Reprogramar evento")
+
+@app.put(
+    "/eventos/reprogramar/{idEvento}", tags=["Eventos"], summary="Reprogramar evento"
+)
 def reprogramarEvento(idEvento: str):
-  return {"message": f"Evento con ID {idEvento} reprogramado"}
+    return {"message": f"Evento con ID {idEvento} reprogramado"}
+
 
 if __name__ == "__main__":
-  uvicorn.run("main:app", port=5000, log_level="info")
+    uvicorn.run("main:app", port=5000, log_level="info")
